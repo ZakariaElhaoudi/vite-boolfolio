c22263025e7ccf37d0491a781d7e73d992bd919e
Zakaria96 <zakarialavoro@gmail.com>
2023-08-21 16:05:57 +0200

    milestone 4

---
+++ src/components/Projects.vue
@@ -0,0 +1,34 @@
+<script>
+import axios from 'axios';
+import { onMounted } from 'vue';
+
+export default {
+  // name: 'Projects',
+  data: function () {
+    return {
+      // projects: []
+    }
+  },
+
+  mounted() {
+    axios.get('http://localhost:8000/api/v1/project-index')
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+}
+
+
+
+
+
+</script>
+
+<template>
+  ciao
+</template>
+
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 // import './style.css'
 import App from './App.vue'
-import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap/dist/js/bootstrap.min.js'
 
 createApp(App).mount('#app')
